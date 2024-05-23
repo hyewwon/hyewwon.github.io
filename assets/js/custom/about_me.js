5b93e70dba782af3ec1dc13af23372d2c34e79dc
hyewwon <rlagp424@gmail.com>
2024-05-23 23:22:49 +0900

    design : about me

--- assets/js/custom/about_me.js
+++ assets/js/custom/about_me.js
@@ -1,9 +1,12 @@
 $().ready(function () {
-  $("#alertStart").click(function () {
-      Swal.fire({           // Alert 타입
-          title: 'Alert가 실행되었습니다.',         // Alert 제목
-          text: '이곳은 내용이 나타나는 곳입니다.',  // Alert 내용
-          showConfirmButton : false       
-      });
-  });
+  $("#enjo-eat").click(function(){
+    var temp = document.createElement("p")
+    temp.innerHTML = "hi"
+    Swal.fire({
+      template:"#enjo-eat-modal"
+  
+    })
+  })
 });
+
+
