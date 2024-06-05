4c5375b0b1cd05ef2330f8e1d9640f1005e758fd
hyewwon <rlagp424@gmail.com>
2024-06-05 17:57:40 +0900

    design : about me

--- assets/js/custom/about_me.js
+++ assets/js/custom/about_me.js
@@ -8,6 +8,33 @@ $().ready(function () {
     })
   })
 
+  $("#emax").click(function(){
+    Swal.fire({
+      template:"#emax-modal",
+      width: 1300,
+      confirmButtonColor: "rgb(60, 179, 113)",
+      confirmButtonText : "Close"
+    })
+  })
+  
+  $("#build-performance").click(function(){
+    Swal.fire({
+      template:"#build-performance-modal",
+      width: 1300,
+      confirmButtonColor: "rgb(60, 179, 113)",
+      confirmButtonText : "Close"
+    })
+  })
+
+  $("#envisager").click(function(){
+    Swal.fire({
+      template:"#envisager-modal",
+      width: 1300,
+      confirmButtonColor: "rgb(60, 179, 113)",
+      confirmButtonText : "Close"
+    })
+  })
+
   $("#buildpay").click(function(){
     Swal.fire({
       template:"#buildpay-modal",
