f4733735c1ec7ab81395ff1fc1390583ee1d3da4
hyewwon <rlagp424@gmail.com>
2024-06-21 16:27:52 +0900

    desing :  about me

--- assets/js/custom/about_me.js
+++ assets/js/custom/about_me.js
@@ -1,4 +1,10 @@
 $().ready(function () {
+  
+  const gnc_month = calculateMonthsDiff("2022-08-18", "재직중");
+  document.querySelectorAll(".gnc-month").forEach(element => {
+      element.innerHTML = gnc_month
+  });
+
   $("#my-career").click(function(){
     Swal.fire({
       template:"#my-career-modal",
@@ -52,4 +58,26 @@ $().ready(function () {
       confirmButtonText : "Close"
     })
   })
+
 });
+
+
+function calculateMonthsDiff(start_str, end_str){
+  let start_date = new Date(start_str);
+  let end_date = end_str == "재직중" ? new Date() : new  Date(end_str);
+  let yearsDifference = end_date.getFullYear() - start_date.getFullYear();
+  let monthsDifference = end_date.getMonth() - start_date.getMonth();
+
+  if (monthsDifference < 0) {
+      yearsDifference--;
+      monthsDifference += 12;
+  }
+
+  const totalMonths = yearsDifference * 12 + monthsDifference;
+
+  if(yearsDifference <= 0 ){
+    return `약 ${monthsDifference} 개월`
+  }else{
+    return `약 ${yearsDifference} 년 ${monthsDifference} 개월`
+  }
+}
